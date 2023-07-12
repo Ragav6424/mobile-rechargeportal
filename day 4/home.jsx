8344f62aa9f86b193de916cecf70f3a5aa99d673
Ragav6424 <125951065+Ragav6424@users.noreply.github.com>
2023-07-12 14:35:20 +0530

    Add files via upload

---
+++ day 4/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './style.css'
+export default function Registration() {
+  return (
+    <div>
+      <header>
+        <h2 class="logo">MOBILE RECHARGE</h2>
+        <nav class="navigation">
+          <a href="/">Home</a>
+          <a href="b">About</a>
+          <a href="c">Service</a>
+          <a href="d">Contact</a>
+          <a href="/login">Login</a>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+

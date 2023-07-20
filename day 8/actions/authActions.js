137a7e75a58396ba075fa6ae9624c597ece73562
Ragav6424 <125951065+Ragav6424@users.noreply.github.com>
2023-07-20 10:02:13 +0530

    Add files via upload

---
+++ day 8/actions/authActions.js
@@ -0,0 +1,10 @@
+// actions/authActions.js
+export const setEmail = (email) => ({
+  type: 'SET_EMAIL',
+  payload: email,
+});
+
+export const setPassword = (password) => ({
+  type: 'SET_PASSWORD',
+  payload: password,
+});
---
+++ day 8/reducers/authReducer.js
@@ -0,0 +1,18 @@
+// reducers/authReducer.js
+const initialState = {
+  email: '',
+  password: '',
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_EMAIL':
+      return { ...state, email: action.payload };
+    case 'SET_PASSWORD':
+      return { ...state, password: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
